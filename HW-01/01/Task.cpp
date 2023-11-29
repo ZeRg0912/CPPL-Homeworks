6ebda7d7b8c0a3764c28cdf9a9cc6445352d0c72
ZeRg0912 <serg.021@yandex.ru>
2023-11-29 11:21:45 +0300

    complete HW-01-01

--- HW-01/01/Task.cpp
+++ HW-01/01/Task.cpp
@@ -1,24 +1,11 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-void printArray(vector<int>& arr) {
-	int lastIndex = 0;
-	for (auto num : arr) {
-		cout << num;
-		if (lastIndex != arr.size() - 1) cout << ", ";
-		lastIndex++;
-	}
-	cout << endl;
-}
-
-int main() {
-	setlocale(LC_ALL, "Russian");
-	system("chcp 1251");
-	system("cls");
-	vector<int> inArray;
+void enterIntoArray(vector<int>& arr) {
 	int in;
 	do {
 		do {
@@ -30,16 +17,32 @@ int main() {
 				std::cin.clear();
 				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			}
-			else if (in != 0){
-				inArray.push_back(in);
+			else if (in != 0) {
+				arr.push_back(in);
 				break;
 			}
 			else { break; }
 		} while (!isdigit(in));
 	} while (in != 0);
+}
 
-	printArray(inArray);
+int main() {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	system("cls");
+	vector<int> inArray;
 
+	auto print = [](const int& n) { cout << n << ' '; };
+	
+	enterIntoArray(inArray);
+	cout << "Входные данные: ";
+	for_each(inArray.cbegin(), inArray.cend(), print);
+	cout << endl;
+
+	cout << "Выходные данные: ";
+	std::for_each(inArray.begin(), inArray.end(), [](int& n) { if(n % 2) n *= 3; });
+	for_each(inArray.cbegin(), inArray.cend(), print);
+	cout << endl;
 	inArray.clear();
 
 	return 0;
