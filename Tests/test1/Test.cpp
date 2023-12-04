844926bb1caac48de3f3416698dd3ed7e4a2b5b0
ZeRg0912 <serg.021@yandex.ru>
2023-12-04 10:22:39 +0300

    add HW-2

---
+++ Tests/test1/Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <variant>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int a;
+	if (!(cin >> a)) cout << "bad!\n";
+	else cout << "int: " << a << endl;
+
+	return 0;
+}
+
