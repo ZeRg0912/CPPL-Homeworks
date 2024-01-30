6a69f77131beffd76fe4bb47a566517e5d4bbb16
Сергей Полевой <serg.021@yandex.ru>
2024-01-30 18:47:49 +0300

    fix

--- HW-04/02/main_test2.cpp
+++ HW-04/02/main_test2.cpp
@@ -1,31 +1,6 @@
-#include "List.cpp"
+#include "../List.cpp"
 #include "catch2/catch_test_macros.hpp"
 #include "catch2/catch_session.hpp"
-
-TEST_CASE("Task #1", "[List]") {
-		List list;
-		REQUIRE(list.Empty() == true);
-		REQUIRE(list.Size() == 0);
-
-		int num = 10;
-		for (int i = 0; i < num; i++) {
-			list.PushBack(i);
-		}
-		CHECK(list.Empty() == false);
-		INFO("WRONG SIZE");
-		CHECK(list.Size() == num);
-
-		for (int i = -1; i >= (-10); i--) {
-			list.PushBack(i);
-		}
-		CHECK(list.Empty() == false);
-		INFO("WRONG SIZE");
-		CHECK(list.Size() == 2 * num);
-
-		list.Clear();
-		INFO("NOT CLEARED");
-		REQUIRE(list.Size() == 0);
-}
 
 TEST_CASE("Task #2:", "[List]") {
 	List list;
@@ -45,6 +20,7 @@ TEST_CASE("Task #2:", "[List]") {
 		REQUIRE(list.Size() == i - 1);
 	}
 
+	// Check for rmpty list
 	CHECK(list.PopFront() == -1);
 
 	// Push & Pop Back
@@ -60,6 +36,7 @@ TEST_CASE("Task #2:", "[List]") {
 		REQUIRE(list.Size() == i - 1);
 	}
 
+	// Check for rmpty list
 	CHECK(list.PopBack() == 0);
 
 	list.Clear();
