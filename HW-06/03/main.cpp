6f8bede2af4855c7fc38e7f7e57ec35163ca0d31
ZeRg0912 <serg.021@yandex.ru>
2024-02-07 11:32:24 +0300

    rewrite MyVector

--- HW-06/03/main.cpp
+++ HW-06/03/main.cpp
@@ -1,108 +1,125 @@
 #include <iostream>
 #include <exception>
 
-using namespace std;
-
-template<typename T> class MyOwnVector {
-
-	T* arr;
+template<typename T>
+class MyVector {
+private:
+	T* data;
+	size_t size;
 	size_t capacity;
-	size_t current;
-
 public:
-	MyOwnVector() {
-		arr = new T[1];
-		capacity = 1;
-		current = 0;
+	MyVector() : data(nullptr), size(0), capacity(0) {}
+	explicit MyVector(size_t init_capacity) : data(new T[init_capacity]), size(0), capacity(init_capacity) {}
+	MyVector(std::initializer_list<T> init_list) : size(init_list.size()), capacity(init_list.size()) {
+		data = new T[capacity];
+		std::copy(init_list.begin(), init_list.end(), data);
 	}
 
-	~MyOwnVector() {
-		delete[] arr;
+	~MyVector() {
+		delete[] data;
 	}
 
-	T at(int index) {
-		if (index <= current) {
-			return arr[index];
+	void push_back(const T& value) {
+		if (size >= capacity) {
+			if (capacity == 0) capacity = 1;
+			else capacity *= 2;
+			T* temp_data = new T[capacity];
+			for (size_t i = 0; i < size; i++) {
+				temp_data[i] = data[i];
+			}
+			delete[] data;
+			data = temp_data;
 		}
-		else {
-			throw out_of_range("Out of memory range!\n");
-		}
-	}
-
-	void push_back(T data) {
-		if (current == capacity) {
-			capacity *= 2;
-			T* temp = new T[capacity];
-			for (size_t i = 0; i < capacity; i++) temp[i] = arr[i];
-			delete[] arr;
-			arr = temp;
-		}
-		arr[current] = data;
-		current++;
+		data[size++] = value;
 	}
 
 	void pop_back() {
-		if (current > 0) {
-			if (current == (capacity / 4)) {
-				capacity /= 2;
-				T* temp = new T[capacity];
-				for (size_t i = 0; i < capacity / 2; i++) temp[i] = arr[i];
-				delete[] arr;
-				arr = temp;
+		if (size > 0) size--;
+		else throw std::out_of_range("Nothing to pop, vector is empty");
+	}
+
+	void shrink_to_fit() {
+		if (size < (capacity / 4)) {
+			capacity = size;
+			T* temp_data = new T[capacity];
+			for (size_t i = 0; i < size; i++) {
+				temp_data[i] = data[i];
 			}
-			current--;
-		}
-		else {
-			throw out_of_range("Vector empty!");
+			delete[] data;
+			data = temp_data;
 		}
 	}
 
-	size_t size() const {
-		return current;
+	T& at(size_t index) {
+		if (index < size) return data[index];
+		else throw std::out_of_range("Index out of range");
+	}
+
+	size_t get_size() const{
+		return size;
 	}
 
 	size_t get_capacity() const {
 		return capacity;
 	}
 
-	void print() {
-		for (size_t i = 0; i < current; i++) {
-			if (i != current - 1) cout << arr[i] << ", ";
-			else cout << arr[i] << endl;
-		}
+	T& operator[](size_t index) {
+		if (index < size) return data[index];
+		else throw std::out_of_range("Index out of range");
 	}
 
+	const T& operator[](size_t index) const{
+		if (index < size) return data[index];
+		else throw std::out_of_range("Index out of range");
+	}
 
+	MyVector& operator=(const MyVector& other) {
+		if (this != other) {
+			delete[] data;
+			size = other.size;
+			capacity = other.capacity;
+			data = new T[capacity];
+			for (size_t i = 0; i < size; i++) {
+				data[i] = other.data[i];
+			}
+		}
+		return *this;
+	}
+
+	void print() {
+		std::cout << "Size: " << get_size() << " Capacity: " << get_capacity() << std::endl;
+		for (size_t i = 0; i < size; i++) {
+			if (i != size -1) std::cout << data[i] << ", ";
+			else std::cout << data[i] << std::endl;
+		}
+	}
 };
 
 int main() {
-	MyOwnVector<int> a;
-
+	MyVector<int> a;
+	MyVector<double> b = { 0.2, 1.22, 3.25 };
+	a.print();
+	a.push_back(5);
+	a.print();
 	for (int i = 0; i < 10; i++) {
 		a.push_back(i);
 	}
-
-	cout << a.size() << " " << a.get_capacity() << endl;
-
-	for (int i = 0; i < 8; i++) {
+	a.print();
+	for (int i = 0; i < 10; i++) {
 		a.pop_back();
 	}
+	a.shrink_to_fit();
+	a.print();
 
-	cout << a.size() << " " << a.get_capacity() << endl;
+	a[0] = 2;
+	a.print();
 
-	cout << a.at(1) << " " << a.at(2) << endl;
-
+	std::cout << a.at(0) << std::endl;
 	try {
-		for (int i = 0; i < 8; i++) {
-			a.pop_back();
-		}
-	} catch (const out_of_range& err) {
-		cerr << err.what() << endl;
+		std::cout << a.at(5) << std::endl;
 	}
-	try {
-		a.at(30);
-	} catch (const out_of_range& err) {
-		cerr << err.what() << endl;
+	catch (const std::out_of_range& err) {
+		std::cerr << "Exception caught: " << err.what() << std::endl;
 	}
 
 	return 0;
