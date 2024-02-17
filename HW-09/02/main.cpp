2f8ed8514c69e8f9fb4ff55404beef2609bfb2c9
Сергей Полевой <serg.021@yandex.ru>
2024-02-17 17:31:58 +0300

    comp 02

--- HW-09/02/main.cpp
+++ HW-09/02/main.cpp
@@ -7,12 +7,84 @@ class BigInt {
 private:
 	std::string value;
 public:
-	BigInt(const std::string& num) : value(num) {}
-	BigInt(std::string&& num) : value(std::move(num)) {}
+	// Constructors
+	BigInt(const std::string& num) {
+		value = "";
+		if (!num.empty() && num.find_first_not_of("0123456789") == std::string::npos) value = num;
+		else throw ("ERROR! not number");
+	}
 
-	BigInt& operator=(std::string&& num) {
-		if (value != num) {
-			value = std::move(num);
+	BigInt(unsigned long long num) {
+		value = "";
+		if (num == 0) {
+			value = "0";
+			return;
+		}
+
+		while (num != 0) {
+			value = char('0' + (num % 10)) + value;
+			num /= 10;
+		}
+	}
+
+	BigInt(const BigInt& other) {
+		value = other.value;
+	}
+
+	// Не понимаю почему не работает, просто не присваиваются в value значения
+#if 0 
+	BigInt(const std::string& num) {
+		value = "";
+		for (int i = 0; i < num.size(); i++) {
+			if (!isdigit(num[i])) throw ("ERROR! not number");
+			value.push_back(num[i] - '0');
+		}
+	}
+#endif
+
+	// Getters
+	std::string getValue() const {
+		return value;
+	}
+
+	// Operators
+	bool operator==(const BigInt& other) const {
+		return value == other.value;
+	}
+
+	bool operator!=(const BigInt& other) const {
+		return value != other.value;
+	}
+
+	bool operator<(const BigInt& other) const {
+		if (value.size() != other.value.size()) {
+			return value.size() < other.value.size();
+		}
+		else {
+			return value < other.value;
+		}
+	}
+
+	bool operator>(const BigInt& other) const {
+		if (value.size() != other.value.size()) {
+			return value.size() > other.value.size();
+		}
+		else {
+			return value > other.value;
+		}
+	}
+
+	BigInt& operator=(BigInt&& other) noexcept {
+		if (this != &other) {
+			value = std::move(other.value);
+			other.value = "0";
+		}
+		return *this;
+	}
+
+	BigInt& operator=(BigInt& other) noexcept {
+		if (this != &other) {
+			value = other.value;
 		}
 		return *this;
 	}
@@ -20,51 +92,143 @@ public:
 	BigInt operator+(const BigInt& other) const {
 		std::string result;
 		int carry = 0;
-		size_t left = value.size() - 1;
-		size_t right = other.value.size() - 1;
+		int left = value.size() - 1;
+		int right = other.value.size() - 1;
 
+		// Continue while there are digits in both numbers or there's a carryover
 		while (left >= 0 || right >= 0 || carry) {
+			// Calculate sum of current digits and carryover
 			int sum = carry;
 			if (left >= 0) {
-				sum += value[left--] - '0';
+				sum += value[left] - '0';
+				left--;
 			}
 			if (right >= 0) {
-				sum += other.value[right--] - '0';
+				sum += other.value[right] - '0';
+				right--;
 			}
+
+			// Add units place to result
+			result.insert(result.begin(), (sum % 10) + '0');
 			carry = sum / 10;
-			sum %= 10;
-
-			result.insert(result.begin(), sum + '0');
 		}
-		return BigInt(std::move(result));
+
+		return BigInt(result);
 	}
 
-	BigInt operator*(int mult) const {
-		std::string result;
-		int carry = 0;
+	BigInt operator*(const BigInt& other) const {
+		std::string result(value.size() + other.value.size(), '0');
 
-		for (size_t i = value.size() - 1; i >= 0 || carry; i--) {
-			int temp = carry;
-			if (i >= 0) temp += (value[i] - '0') * mult;
-			carry = temp / 10;
-			temp %= 10;
-			result.insert(result.begin(), temp + '0');
+		for (int i = value.size() - 1; i >= 0; i--) {
+			int carry = 0;
+			for (int j = other.value.size() - 1; j >= 0; j--) {
+				int product = (value[i] - '0') * (other.value[j] - '0') + (result[i + j + 1] - '0') + carry;
+				result[i + j + 1] = product % 10 + '0';
+				carry = product / 10;
+			}
+			result[i] += carry;
 		}
-		return BigInt(std::move(result));
-	}
 
-	friend std::ostream& operator<<(std::ostream& os, const BigInt& big_int) {
-		os << big_int.value;
-		return os;
+		// Delete zero before
+		size_t start_pos = result.find_first_not_of('0');
+		if (start_pos != std::string::npos) {
+			return BigInt(result.substr(start_pos));
+		}
+		else {
+			return BigInt("0");
+		}
 	}
 };
 
-int main() {
-	auto number1 = BigInt("114575");
-	auto number2 = BigInt("78524");
-	auto result = number1 + number2;
+std::ostream& operator<<(std::ostream& os, const BigInt& bigint) {
+	os << bigint.getValue();
+	return os;
+}
 
-	std::cout << result; // 193099
+template<typename T>
+void Mult(const T& num1, const T& num2) {
+	std::cout << "Mmultiply : " << num1 << " * " << num2 << " = " << num1 * num2 << '\n';
+	//return num1 * num2;
+}
+
+template<typename T>
+void Sum(const T& num1, const T& num2) {
+	std::cout << "Sum: " << num1 << " + " << num2 << " = " << num1 + num2 << '\n';
+	//return num1 * num2;
+}
+
+template<typename T>
+void Check(const T& num1, const T& num2) {
+	if (num1 == num2) std::cout << num1 << " = " << num2 << '\n';
+	else if (num1 > num2) std::cout << num1 << " > " << num2 << '\n';
+	else std::cout << num1 << " < " << num2 << '\n';
+
+	//return num1 * num2;
+}
+
+int main() {
+	try {
+		auto number1 = BigInt("114575");
+		auto number2 = BigInt("78524");
+		std::cout << "\033[32mBase section:\033[0m\n";
+		std::cout << "Consttructor with string: number1 = " << number1 << std::endl;
+		std::cout << "Consttructor with string: number2 = " << number2 << std::endl;
+		auto result = number1 + number2;
+		std::cout << "Result of sum: " << result << std::endl; // 193099
+		result = number1 * number2;
+		std::cout << "Result of mult: " << result << std::endl; // 8996887300
+		std::cout << std::string(70, '-') << "\n";
+
+		Sum(number1, number2);
+		Mult(number1, number2);
+		std::cout << std::string(70, '=') << "\n";
+
+		std::cout << "\033[32mConsttructor with num:\033[0m\n";
+		auto number3 = BigInt(5555534534534);
+		std::cout << "number3 = " << number3 << std::endl;
+		std::cout << std::string(70, '=') << "\n";
+
+		std::cout << "\033[32mBool check section:\033[0m\n";
+		Check(number1, number2);
+		Check(number2, number1);
+		Check(number3, number1);
+		Check(number2, number3);
+		Check(number3, number3);
+		std::cout << std::string(70, '=') << "\n";
+
+		std::cout << "\033[32mAssignment w/o move: \033[0m\n";
+		std::cout << "before assignment: number1 = "
+			<< number1
+			<< " & number2 = "
+			<< number2
+			<< std::endl;
+		number1 = number2;
+		std::cout << "After assignment: number1 = " 
+			<< number1
+			<< " & number2 = "
+			<< number2
+			<< std::endl;
+		std::cout << std::string(70, '=') << "\n";
+
+		number1 = 114575;
+
+		std::cout << "\033[32mAssignment with move: \033[0m\n";
+		std::cout << "before move assignment: number1 = "
+			<< number1
+			<< " & number2 = "
+			<< number2
+			<< std::endl;
+		number1 = std::move(number2);
+		std::cout << "After move assignment: number1 = "
+			<< number1
+			<< " & number2 = "
+			<< number2
+			<< std::endl;
+		std::cout << std::string(70, '=') << "\n";
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	return 0;
 }
